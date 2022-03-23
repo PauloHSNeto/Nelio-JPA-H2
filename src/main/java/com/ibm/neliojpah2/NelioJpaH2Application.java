7ca4b88eed21c546aad00481cbc44cc854ee12a1
Admin <paulo.silva1989/@hotmail.com>
2022-03-23 09:29:16 -0300

    first commit

---
+++ src/main/java/com/ibm/neliojpah2/NelioJpaH2Application.java
@@ -0,0 +1,13 @@
+package com.ibm.neliojpah2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NelioJpaH2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NelioJpaH2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/ibm/neliojpah2/NelioJpaH2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibm.neliojpah2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NelioJpaH2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
